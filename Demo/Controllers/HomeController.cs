0965a6829a29db208ab5c02940d151cc990b0f80
z1z234z <634778353@qq.com>
2018-07-04 20:54:01 +0800

    change

--- Demo/Controllers/HomeController.cs
+++ Demo/Controllers/HomeController.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Demo.Models;
 using Demo.Service;
-using Microsoft.AspNetCore.Http;
-using System.IO;
-using System.Threading;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Hosting;
+
 
 namespace Demo.Controllers
 {
@@ -38,49 +34,9 @@ namespace Demo.Controllers
             return View();
         }
 
-        public IActionResult Test()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        public class TemUrl
-        {
-            public int id { get; set; }
-            public string url { get; set; }
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> OnPostUpload([FromServices]IHostingEnvironment environment)
-        {
-            List<TemUrl> list = new List<TemUrl>();
-            var files = Request.Form.Files;
-            string webRootPath = environment.WebRootPath;
-            string contentRootPath = environment.ContentRootPath;
-            foreach (var formFile in files)
-            {
-                if (formFile.Length > 0)
-                {
-                    var fileName = Guid.NewGuid().ToString() + ".jpg";
-                    var path = Path.Combine(environment.WebRootPath + "\\images\\upload", fileName);
-                    using (var stream = new FileStream(path, FileMode.CreateNew))
-                    {
-                        await formFile.CopyToAsync(stream);
-                        TemUrl tu = new TemUrl();
-                        tu.url = @"/images/upload/" + fileName;
-                        list.Add(tu); ;
-                    }
-                }
-            }
-
-            return new JsonResult(list);
-
         }
     }
 }
--- Demo/Controllers/ReleaseMissingController.cs
+++ Demo/Controllers/ReleaseMissingController.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Threading;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Hosting;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,6 +19,39 @@ namespace Demo.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public class TemUrl
+        {
+            public int id { get; set; }
+            public string url { get; set; }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> OnPostUpload([FromServices]IHostingEnvironment environment)
+        {
+            List<TemUrl> list = new List<TemUrl>();
+            var files = Request.Form.Files;
+            string webRootPath = environment.WebRootPath;
+            string contentRootPath = environment.ContentRootPath;
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var fileName = Guid.NewGuid().ToString() + ".jpg";
+                    var path = Path.Combine(environment.WebRootPath + "\\images\\upload", fileName);
+                    using (var stream = new FileStream(path, FileMode.CreateNew))
+                    {
+                        await formFile.CopyToAsync(stream);
+                        TemUrl tu = new TemUrl();
+                        tu.url = @"/images/upload/" + fileName;
+                        list.Add(tu); ;
+                    }
+                }
+            }
+
+            return new JsonResult(list);
+
         }
     }
 }
