ef0fb2252ca002f8d7dc084e8919343bb8d4a2b7
z1z234z <634778353@qq.com>
2018-07-05 21:24:01 +0800

    2

--- Demo/Controllers/MainController.cs
+++ Demo/Controllers/MainController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Demo.Service;
 using Demo.Models;
+using System.Collections;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -29,16 +30,24 @@ namespace Demo.Controllers
         public IActionResult getType()
         {
             int code = 200;
-            List<LoseType> list = new List<LoseType>();
+            List<Hashtable> typelist = new List<Hashtable>();
             //前端向后端发送数据
-            list = service.GetLoseTypes();
+            List<LoseType> list = service.GetLoseTypes();
+            for(int i = 0; i < list.Count; i++)
+            {
+                Hashtable table = new Hashtable();
+                table.Add("index", i);
+                table.Add("type", list[i].Name);
+                table.Add("children", new List<Hashtable>());
+                typelist.Add(table);
+            }
             if (list == null)
             {
                 code = 500;
             }
             return Ok(new
             {
-                typelist = list,
+                typelist = typelist,
                 code = code
             });
         }
