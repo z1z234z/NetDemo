54fc1ec3ccfa97e836e8248095b875a5170f861f
z1z234z <634778353@qq.com>
2018-07-07 02:33:00 +0800

    5

--- Demo/Service/HomeService.cs
+++ Demo/Service/HomeService.cs
@@ -17,12 +17,20 @@ namespace Demo.Service
 
         private readonly UserDao userDao;
 
+        private readonly ReplyDao replyDao;
+
+        private readonly PrivateMessageDao privateMessageDao;
+
+        private readonly AttentionDao attentionDao;
+
         public HomeService(DBContext context)
         {
             loseTypesDao = new LoseTypesDao(context);
             ownerDao = new OwnerDao(context);
             finderDao = new FinderDao(context);
-            userDao = new UserDao(context);
+            replyDao = new ReplyDao(context);
+            privateMessageDao = new PrivateMessageDao(context);
+            attentionDao = new AttentionDao(context);
         }
 
         public User getUserInfo(String account)
@@ -33,6 +41,51 @@ namespace Demo.Service
                 user = userDao.Select(null, account, null, null, null, null, null, null, null, null, null, null, null)[0];
             }
             return user;
+        }
+
+        public List<Owner> getOwnerByUser(User user)
+        {
+            return ownerDao.Select(null, user, null, null, null, null, null, null, null, 0);
+        }
+
+        public List<Finder> getFinderByUser(User user)
+        {
+            return finderDao.Select(null, user, null, null, null, null, null, null, null, null, 0);
+        }
+
+        public List<Reply> getReplyByUser(User user)
+        {
+            return replyDao.Select(null, null, null, user, null);
+        }
+
+        public List<PrivateMessage> getPMByUser(User user)
+        {
+            return privateMessageDao.Select(null, null, user, null, null, null);
+        }
+
+        public Owner getOwnerById(int id)
+        {
+            Owner owner = null;
+            if (ownerDao.Select(id, null, null, null, null, null, null, null, null, 0).Count > 0)
+            {
+                owner = ownerDao.Select(id, null, null, null, null, null, null, null, null, 0)[0];
+            }
+            return owner;
+        }
+
+        public Finder getFinderById(int id)
+        {
+            Finder finder = null;
+            if (finderDao.Select(id, null, null, null, null, null, null, null, null, null, 0).Count > 0)
+            {
+                finder = finderDao.Select(id, null, null, null, null, null, null, null, null, null, 0)[0];
+            }
+            return finder;
+        }
+
+        public bool editProfile(User user)
+        {
+            return userDao.Edit(user);
         }
     }
 }
